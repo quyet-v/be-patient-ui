0573e96ad2033c4935401e2c1709b18ad83cafce
qv5 <qv5@students.waikato.ac.nz>
2025-03-24 00:18:23 +1300

    BEP-9 Added register page.

--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import {SubmitHandler, useForm} from 'react-hook-form';
 import {AuthLayout} from "../layouts/auth-layout.tsx";
 import {Button, TextInput} from "@mantine/core";
-import {useMutation} from "@tanstack/react-query";
-import axios from "axios";
-import {useNavigate} from "react-router";
+import {NavLink} from "react-router";
+import useLogin from "../../hooks/use-login.ts";
 
 type FormFields = {
     email: string,
@@ -12,19 +11,14 @@ type FormFields = {
 }
 
 export const LoginForm = () => {
-    const navigate = useNavigate();
     const {register, handleSubmit} = useForm<FormFields>();
-    const { isPending, isSuccess,mutate } = useMutation({
-        mutationFn: (loginData: FormFields) => {
-            return axios.post(`${import.meta.env.VITE_API_URL}/login?useCookies=true&useSessionCookies=true`, loginData)
-        },
-    })
+    const { isPending,login } = useLogin();
 
     const onSubmit: SubmitHandler<FormFields> = (data) => {
-        mutate(data, {onSuccess: () => navigate("/")});
+        login(data, "/");
     }
 
-    return <AuthLayout title="Log in to your account">
+    return <AuthLayout title="Log in to your account" redirect={<NavLink to={"/auth/register"}>Register</NavLink>}>
         <form className={"space-y-6"} onSubmit={handleSubmit(onSubmit)}>
             <TextInput {...register("email", {required: "Email is required"})} label="Email" type="email" />
             <TextInput {...register("password", {required: "Password is required"})} label="Password" type="password" />
